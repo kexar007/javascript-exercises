561507290756225ba959bc5edfedd1cedb271fad
Rakesh <kexar007@gmail.com>
2023-10-08 18:43:44 +0530

    do ex02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    strng="";
+    if (times<0) {
+        return "ERROR";
+    }else{
+        for (let i = 0; i < times; i++) {
+            strng+=word;
+            
+        }
+        return strng;}
 };
 
 // Do not edit below this line
