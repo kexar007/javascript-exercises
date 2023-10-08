2274e31b6f18492ae5d1f5617fadc423eeae8df9
Rakesh <kexar007@gmail.com>
2023-10-09 02:01:08 +0530

    do 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(marray,...args) {
+
+   let result =[];
+   let j=0;
+  args.forEach(x => {
+    
+    function remover(value,index,arr){
+        if (x===value) {
+            arr.splice(index,1)
+        }
+
+
+    };
+
+
+
+    let r = marray.filter(remover);
+
+
+
+
+  });
+  console.log(args)
+  console.log(marray)
+  return marray;
 
 };
 
