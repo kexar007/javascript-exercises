cd0d8e2ede22f5d5953282877f1c11cd1045ceae
Rakesh <kexar007@gmail.com>
2023-10-08 20:08:10 +0530

    do ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    let length=string.length;
+    for (let i = length; i >=0; i--) {
+        reverse+= string.charAt(i);
+       
+        
+    }
+    return reverse;
 };
 
 // Do not edit below this line
