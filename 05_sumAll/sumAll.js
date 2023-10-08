f161495a57b6760cd94da8e1225f2b63edeb241b
Rakesh <kexar007@gmail.com>
2023-10-09 03:10:40 +0530

    do ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    if (((a||b)<0)||(typeof(a)!=="number")||(typeof b !=="number" )) {
+
+
+        return `ERROR`;
+        
+    } else {
+     function allsum (n){
+        return (n*(n+1))/2;
+     }  ;
+     if (a>b) {
+        return allsum(a)-allsum(b-1);
+        
+     } else {
+        return allsum(b)-allsum(a-1);
+        
+     }
+       
+    }
 
 };
 
