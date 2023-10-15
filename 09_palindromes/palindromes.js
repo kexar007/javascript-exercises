9b0c8dd7b2f0dfb577dbfb389df6795c3ac6ed48
Rakesh <kexar007@gmail.com>
2023-10-15 16:46:45 +0530

    do ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str= str.replace(/[^\w\s]/gi, '');
+    str= str.toLowerCase();
+    str = str.replace(/\s+/g, '');
+    console.log(str)
+    return str===str.split("").reverse().join("");
 };
 
 // Do not edit below this line
