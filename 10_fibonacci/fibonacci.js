9bb13dd36f6cbf485e9f22b4c357762264146eaa
Rakesh <kexar007@gmail.com>
2023-10-15 19:42:59 +0530

    do ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num<=0) {
+        return "OOPS";
+        
+    } else {
 
+        let prev2=0;
+        let prev1 =1;
+        let i =1;
+        let current=1;
+        
+        while (i<num) {
+           
+            current=prev1+prev2;
+            prev2=prev1;
+            prev1=current;
+            i++;
+            
+            
+        }
+        return current;
+    }
 };
 
 // Do not edit below this line
