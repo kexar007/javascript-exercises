e7891687d8d3d6f50e0637594ab9af9acbbe13da
Rakesh <kexar007@gmail.com>
2023-10-16 01:08:49 +0530

    do ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const d = new Date();
+    let year = d.getFullYear();
+
+    for (let i = 0; i < people.length; i++) {
+        if (!people[i].yearOfDeath) {
+            people[i].age=year-people[i].yearOfBirth;
+        } else {
+           people[i].age=people[i].yearOfDeath-people[i].yearOfBirth; 
+        }
+        
+    }
+    
+
+    const oldest = people.reduce(
+        (oldest,current) => {
+          
+
+
+            return oldest.age > current.age ? oldest : current ;
+        }
+
+
+
+
+
+    )
+return oldest;
+
+
 
 };
 
